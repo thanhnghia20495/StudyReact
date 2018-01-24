63d83538ba2884a718116fc28a2dee088694afb0
Pham Thanh Nghia (CME.TEST) <NghiaPT7@fsoft.com.vn>
2018-01-24 16:44:15 +0700

    study state and ref

--- src/App.js
+++ src/App.js
@@ -4,34 +4,77 @@ import Header from './components/Header';
 import Product from './components/Product';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [{
+        id: 1,
+        nameProduct: 'Iphone 6 plus',
+        price: 16000000,
+        status: true
+      }, {
+        id: 2,
+        nameProduct: 'Iphone 7 plus',
+        price: 26000000,
+        status: true
+      }, {
+        id: 3,
+        nameProduct: 'Iphone 7',
+        price: 36000000,
+        status: true
+      }, {
+        id: 4,
+        nameProduct: 'Iphone 8 plus',
+        price: 46000000,
+        status: true
+      }, {
+        id: 5,
+        nameProduct: 'Iphone 8',
+        price: 56000000,
+        status: true
+      }],
+      isActive: true
+    }
+    this.onAddProduct = this.onAddProduct.bind(this);
+    this.modifyActive = this.modifyActive.bind(this);
+  }
+  eventClick(text) {
+    alert(text);
+    console.log(text);
+  }
+  onAddProduct() {
+    console.log(this.refs.name.value);
+  }
+  modifyActive() {
+    var str=JSON.parse(JSON.stringify(this.state.products));
+
+    // if (this.state.isActive === true) {
+    //   this.setState({
+    //     isActive:false
+    //   })
+    // } else {
+    //   this.setState({
+    //     isActive:true
+    //   })
+    // }
+    // this.state.products.map((product,index)=>{
+    //     this.setState({
+    //       status:false
+    //     })
+    // })
+    // this.setState({
+    //   isActive:!this.state.isActive
+    // })
+    // for (var i = 0; i < this.state.products.length; i++) 
+    // { 
+    //   if (this.state.products[i].id === 2) { 
+    //     this.setState({ 
+    //       status : false 
+    //     }) } }
+        console.log(str);
+  }
   render() {
-    var products = [{
-      id: 1,
-      nameProduct: 'Iphone 6 plus',
-      price: 16000000,
-      status: true
-    }, {
-      id: 2,
-      nameProduct: 'Iphone 7 plus',
-      price: 26000000,
-      status: false
-    }, {
-      id: 3,
-      nameProduct: 'Iphone 7',
-      price: 36000000,
-      status: false
-    }, {
-      id: 4,
-      nameProduct: 'Iphone 8 plus',
-      price: 46000000,
-      status: true
-    }, {
-      id: 5,
-      nameProduct: 'Iphone 8',
-      price: 56000000,
-      status: true
-    }];
-    let pr = products.map((product, index) => {
+    let pr = this.state.products.map((product, index) => {
       let result = '';
       if (product.status) {
         result = <Product price={product.price} key={product.id} >{product.nameProduct} {product.id}</Product>
@@ -41,10 +84,23 @@ class App extends Component {
     return (
       <div>
         <Header />
-        <div className="row">
-          <div className="col-xs-6 col-sm-12 col-md-6 col-lg-6">
-            {pr}
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div className="form-group">
+                <label>Product Name</label>
+                <input type="text" className="form-control" placeholder="Input field" ref="name" />
+              </div>
+              <button type="submit" className="btn btn-primary" onClick={this.onAddProduct}>Insert Data</button>
+            </div>
           </div>
+          <br />
+          <div className="row">
+            <div className="col-xs-6 col-sm-12 col-md-6 col-lg-6">
+              {pr}
+            </div>
+          </div>
+          <button type="button" className="btn btn-warning" onClick={this.modifyActive}>Click Me : {this.state.isActive === true ? 'true' : 'false'}</button>
         </div>
       </div>
     );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 class Product extends Component {
-  render() { 
+
+  constructor(props) {
+    super(props);
+    this.buyProduct = this.buyProduct.bind(this);
+  }
+
+  buyProduct() {
+    alert("Buy Success! " + this.props.children);
+  }
+
+
+  render() {
     return <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-    <div className="thumbnail">
-      <img src="https://cellphones.com.vn/media/catalog/product/cache/7/image/500x500/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-6-plus-gold_11.png" alt="Iphone" />
-      <div className="caption">
-        <h3>{this.props.children}</h3>
-        <p>{this.props.price} VNĐ</p>
-        <button type="button" className="btn btn-success" >Mua Hàng</button>
+      <div className="thumbnail">
+        <img src="https://cellphones.com.vn/media/catalog/product/cache/7/image/500x500/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-6-plus-gold_11.png" alt="Iphone" />
+        <div className="caption">
+          <h3>{this.props.children}</h3>
+          <p>{this.props.price} VNĐ</p>
+          <button type="button" className="btn btn-success" onClick={this.buyProduct} >Mua Hàng</button>
+        </div>
       </div>
     </div>
-  </div>
   }
 }
 
